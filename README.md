d533467eb494fa81ef18bebc4ec2315e10ab70ea
LUCKYINS <elhusseinabdalrahman@gmail.com>
2024-11-12 00:13:34 +0100

    org:/ README.md + main.cpp
    READ.md:
        - ajouté mes sources
    main.cpp:
        - supprimé  code no important

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # OS_Projet_1 (not finished)
  Projet pour le cours d'OS (INFO-F201), ULB, novembre 2024
+
+# Sources
+  - Pour bash
+
+    - OS: TP2
+
+    - https://stackoverflow.com/
+
+    - https://devhints.io/bash
+
+  - Pour c/c++
+
+    - OS: TP3, TP5
+
+    - https://stackoverflow.com/
+
+    - https://www.geeksforgeeks.org/named-pipe-fifo-example-c-program/
--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-using std::cout, std::string, std::cerr, std::unordered_map, std::endl, std::unordered_set, std::cin;
+using std::cout, std::string, std::cerr, std::unordered_map, std::endl, std::unordered_set;
 //TODO : à voir si on met le namespace dans un hpp, et si on rajoute des fichiers. Il faudra relire les consignes à ce sujet.
 
 namespace ExceptionHandler{
@@ -29,14 +29,11 @@ namespace ExceptionHandler{
         //RESERVED_EXPRESSION_USERNAME = 4 //Si l'utilisateur s'appelle '.', '..', etc.
     };
 
-    string error_color = "\x1B[31m";
-    string end_color = "\x1B[0m";
-
     //On va probablement devoir raffiner ces exceptions, en disant par exemple lequel des deux usernames présentés est fautif.
     const unordered_map<ExceptionHandler::ExitCodes, string> EXIT_CODE_MESSAGES = {
         {LACKING_USERNAME, "chat pseudo_utilisateur pseudo_destinataire [--bot] [--manuel]"},
-        {USERNAME_TOO_LONG, error_color+"Un nom d'utilisateur est trop long (> 30 )"+end_color},
-        {INVALID_CHAR_IN_USERNAME, error_color+"Un nom d'utilisateur ne peut contenir un caractère interdit ('[', ']', '-', '/') or étre ('.', '..')"+end_color},
+        {USERNAME_TOO_LONG, "Un nom d'utilisateur est trop long (> 30 )"},
+        {INVALID_CHAR_IN_USERNAME, "Un nom d'utilisateur ne peut contenir un caractère interdit ('[', ']', '-', '/') or étre ('.', '..')"},
         //{RESERVED_EXPRESSION_USERNAME, error_color+"Un nom d'utilisateur ne peut être un mot réservé (comme '.' ou '..')"+end_color}
         //etc...
     };
