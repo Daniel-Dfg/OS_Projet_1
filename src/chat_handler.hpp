c489ad90d06dd57cda6ea492f0401cd6b3aad12b
Onur Dogan <onurdogancs@gmail.com>
2024-11-20 21:09:01 +0100

    add/: shared memory and signal handling

--- src/chat_handler.hpp
+++ src/chat_handler.hpp
@@ -13,12 +13,17 @@ Les signaux qui en découlent sont gérés dans une fonction à adapter TODO
 #include <unistd.h>
 #include <dirent.h>
 #include <signal.h>
-#include <sys/shm.h> //TODO => avant fork
+//#include <sys/shm.h> //TODO => avant fork
+#include<sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <queue>
 #include "exception_handler.hpp"
 
+extern std::string g_path_from_user1;
+extern std::string g_path_from_user2;
+extern int g_file_desc1;
+extern int g_file_desc2;
 
 class ChatHandler{
     private:
@@ -38,15 +43,20 @@ class ChatHandler{
     int file_desc1, file_desc2;
 
     const bool bot, manuel;
+    std::queue<string>* shared_memory_ptr;
+    const size_t shared_memory_size = 4096;
 
     int send_message(char (&thing)[BUFFER_SIZE]); //renvoie : nombre de bytes envoyés si tout s'est bien passé, -1 sinon.
     int receive_message(char (&received_message)[BUFFER_SIZE]); //même idée que pour send_message
     void display_pending_messages();
     public:
-    ChatHandler(const string &username1_, const string &username2_, const bool &bot_, const bool &manuel_);
 
+    ChatHandler(const string &username1_, const string &username2_, const bool &bot_, const bool &manuel_);
+    ~ChatHandler();
     void access_sending_channel(const string &recipient);
     void access_reception_channel(const string &sender);
+    std::queue<std::string>* init_shared_memory_block();
+    void remove_shared_memory_block();
 };
 
 #endif // CHAT_HANDLER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,39 @@
 #include "exception_handler.hpp"
 #include "chat_handler.hpp"
 
+#include <iostream>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <signal.h>
 #include <sys/shm.h> //TODO => avant fork
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <cstdio>
 
-void signal_handler(const int sig){ //TODO : bouger ceci à un endroit pertinent
-    if (sig == 0){
-        //...
-    }
+void sigpipe_signal_handler(const int sig){ //TODO : bouger ceci à un endroit pertinent
+    printf("The other user closed the chat. pipesig\n");
     exit(0);
 }
+void sigint_signal_handler(const int sig){
+    printf("Exiting now... sigint\n");
+    // Close the communication channels before removing the files
+    close(g_file_desc1);
+    close(g_file_desc2);
+    std::remove(g_path_from_user1.c_str());
+    std::remove(g_path_from_user2.c_str());
 
+    exit(0);
+}
 int main(int argc, char* argv[]) {
+
     bool bot = false;
     bool manuel = false;
-    ExceptionHandler::process_args(argc, argv, bot, manuel);
 
+    ExceptionHandler::process_args(argc, argv, bot, manuel);
+    
     std::string* user1_name = new std::string(argv[1]);
     std::string* user2_name = new std::string(argv[2]);
 
@@ -28,11 +41,11 @@ int main(int argc, char* argv[]) {
 
     // Séparation en deux processus
     int process = fork();
-
     // Communication avec deux processus (Original: envoi de messages, Secondaire: réception de messages)
     // Il faut avoir 2 terminaux (terminal1: ./chat A B, terminal2: ./chat B A par ex.)
     if (process > 0) { // Père
-        signal(SIGINT, signal_handler);
+        signal(SIGPIPE, sigpipe_signal_handler);
+        signal(SIGINT, sigint_signal_handler);  
         chat.access_sending_channel(*user2_name);
     } else { // Fils
         chat.access_reception_channel(*user2_name);
