c4c69e02934d17d59904626ef19468375bbca870
LUCKYINS <elhusseinabdalrahman@gmail.com>
2024-11-28 17:14:24 +0100

    Revert "fix/: memory init crash and merge my changes with the current changes"
    This reverts commit c6cb497056e1cace6c441430993046ec703456b1.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,7 @@
 #include <signal.h>
 #include <unistd.h>
 
-//using namespace ChatGlobals;
-ChatHandler* ChatHandler::current_instance = nullptr;  // Initialize to nullptr
+using namespace ChatGlobals;
 
 int main(int argc, char* argv[]) {
 
@@ -14,19 +13,13 @@ int main(int argc, char* argv[]) {
 
     ExceptionHandler::process_args(argc, argv, bot, manuel);
 
-    
+
     std::string* user1_name = new std::string(argv[1]);
     std::string* user2_name = new std::string(argv[2]);
-    
-    /*
-    std::string user1_name = argv[1];
-    std::string user2_name = argv[2];
-    */
 
-    ChatHandler chat_handler(*user1_name, *user2_name, bot, manuel);
     //ChatHandler chat = ChatHandler(*user1_name, *user2_name, bot, manuel);
-    //ChatGlobals::g_chat_handler = new ChatHandler(*user1_name, *user2_name, bot, manuel);
-    //ChatGlobals::g_chat_handler = &chat;
+    ChatGlobals::g_chat_handler = new ChatHandler(*user1_name, *user2_name, bot, manuel);
+
 
     // Séparation en deux processus
     pid_t process = fork();
@@ -34,14 +27,13 @@ int main(int argc, char* argv[]) {
 
     if (process > 0) { // Père
         signal(SIGINT, Signal_Handler);
-        //ChatGlobals::g_chat_handler->access_sending_channel(*user2_name);
-        chat_handler.access_sending_channel(*user2_name);
+        ChatGlobals::g_chat_handler->access_sending_channel(*user2_name);
+
     } else { // Fils
         signal(SIGINT, SIG_IGN);
-        //ChatGlobals::g_chat_handler->access_reception_channel(*user2_name);
-        chat_handler.access_reception_channel(*user2_name);
+        ChatGlobals::g_chat_handler->access_reception_channel(*user2_name);
     }
-    //delete ChatGlobals::g_chat_handler;
+    delete ChatGlobals::g_chat_handler;
     delete user1_name;
     delete user2_name;
 
